19ec8b05a8e950f03f052241b8cf3311011cb721
Aran <a.farshy@gmail.com>
2020-06-19 04:08:33 +0200

    Updates

---
+++ precache-manifest.85f6948bd55ed5cd9727b4f61309520d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cff64e22e8ef462f6cd321fa71ea53e",
+    "url": "/ReactRechner/index.html"
+  },
+  {
+    "revision": "802fc93f3be2300dd267",
+    "url": "/ReactRechner/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b22edad888d7e946f15a",
+    "url": "/ReactRechner/static/css/main.8a7ee7fe.chunk.css"
+  },
+  {
+    "revision": "802fc93f3be2300dd267",
+    "url": "/ReactRechner/static/js/2.cf11ec95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRechner/static/js/2.cf11ec95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22edad888d7e946f15a",
+    "url": "/ReactRechner/static/js/main.ad748007.chunk.js"
+  },
+  {
+    "revision": "22a50709ef758f167ef7",
+    "url": "/ReactRechner/static/js/runtime-main.d2231c74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactRechner/static/media/logo.5d5d9eef.svg"
+  }
+]);
